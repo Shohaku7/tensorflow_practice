41cea950ba18a1a1358636f68ec8f081205b2e35
hushuai <1677322785@qq.com>
2022-02-06 21:11:15 +0800

    tensorflow

--- other/lihang_book_algorithm/AdaBoost/adaboost_cpp.py
+++ other/lihang_book_algorithm/AdaBoost/adaboost_cpp.py
@@ -12,8 +12,8 @@ import ctypes
 import logging
 import numpy as np
 import pandas as pd
-
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 sign_time_count = 0
